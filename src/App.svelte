06ec9b857a9a16a86194a715a9f79d00d08e94b5
SamoStraka <samostraka@gmail.com>
2021-07-10 23:07:06 +0200

    Starting and Main heading

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Welcome</h1>
+	<h2>This is</h2>
+	<h3>Typing Speed Test</h3>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
@@ -18,8 +20,28 @@
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
-		font-size: 4em;
+		font-size: 3em;
 		font-weight: 100;
+		margin: 0 0 -.4em 0;
+		padding: 0;
+	}
+
+	h2 {
+		color: #dddddd;
+		font-size: 1.5em;
+		font-weight: 200;
+		text-transform: uppercase;
+		margin: 0;
+		padding: 0;
+	}
+
+	h3 {
+		color: #ff3e00;
+		font-size: 6em;
+		font-weight: 900;
+		text-transform: uppercase;
+		margin: -.45em 0 0 0;
+		padding: 0;
 	}
 
 	@media (min-width: 640px) {
